6474bdd5dd8a2d523a278abc15974cf446b62319
wangpipi1991 <xuesenwang1991@gmail.com>
2024-10-29 20:43:04 +0100

    some css changes made in DatabaseTableMetadata.vue;

--- src/pages/DatabaseTableMetadata.vue
+++ src/pages/DatabaseTableMetadata.vue
@@ -1,19 +1,26 @@
 <template>
   <div class="database-table-metadata-container">
-    <h2>All Tables' Metadata</h2>
+    <h2 class="title">All Tables' Metadata</h2>
     <div v-if="allTablesMetadata && allTablesMetadata.length > 0">
       <v-data-table
+        density="compact"
         :headers="tableHeaders"
-        :items="allTablesMetadata">
+        :items="allTablesMetadata"
+      >
+        <template v-slot:item.tableName="{ item }">
+          <h3>{{ item.tableName }}</h3>
+        </template>
         <template v-slot:item.columns="{ item }">
           <v-data-table
+            density="compact"
             :headers="columnHeaders"
-            :items="item.columns">
+            :items="item.columns"
+          >
             <template v-slot:item.columnName="{ item }">
               <div>
-                <div>{{ item.columnName }}</div>
-                <div v-if="item.primaryKey">(<b>PK</b>)</div>
-                <div v-if="item.foreignKeyMetadata">(<b>FK</b> reference to <b>{{ item.foreignKeyMetadata.pkTableName }}.{{ item.foreignKeyMetadata.pkColumnName }}</b>)</div>
+                <span>{{ item.columnName }}</span>
+                <span v-if="item.primaryKey">(<b style="color: red">PK</b>)</span>
+                <span v-if="item.foreignKeyMetadata">(<b style="color: blue">FK</b> reference to <b>{{ item.foreignKeyMetadata.pkTableName }}</b>.{{ item.foreignKeyMetadata.pkColumnName }})</span>
               </div>
             </template>
           </v-data-table>
@@ -31,14 +38,14 @@ import { useDatabaseConnectorStore } from "@/stores/databaseConnectorStore";
 export default {
   data: () => ({
     tableHeaders: [
-      { title: 'Table Name', key: 'tableName', align: 'center', width: '10%', sortable: true },
-      { title: 'Table Columns', key: 'columns', align: 'center', width: '90%', sortable: false },
+      { title: 'Table Name', key: 'tableName', align: 'center', width: '20%', sortable: true },
+      { title: 'Table Columns', key: 'columns', align: 'start', width: '80%', sortable: false },
     ],
     columnHeaders: [
-      { title: 'Column Name', key: 'columnName', align: 'center' },
-      { title: 'Column Type', key: 'columnType', align: 'center' },
-      { title: 'Column Size', key: 'columnSize', align: 'center' },
-      { title: 'Column Default Value', key: 'columnDefaultValue', align: 'center'}
+      { title: 'Column Name', key: 'columnName', align: 'start', width: '20%' },
+      { title: 'Column Type', key: 'columnType', align: 'start', width: '20%' },
+      { title: 'Column Size', key: 'columnSize', align: 'start', width: '20%' },
+      { title: 'Column Default Value', key: 'columnDefaultValue', align: 'start', width: '20%'}
     ]
   }),
   computed: {
@@ -53,8 +60,11 @@ export default {
 <style scoped>
 /* Add some basic styling */
 .database-table-metadata-container {
-  margin: auto;
-  text-align: center;
+  .title {
+    text-align: center;
+    margin-bottom: 1em;
+    margin-top: 1em;
+  }
   .v-data-table {
     border: 1px solid #ddd;
   }
